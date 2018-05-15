974484af65e887c290bc26a5ae5db5db0b106235
Courtenay <court.eccles@gmail.com>
2018-05-14 22:31:53 -0600

    one div with animation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component {
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      spin: false
+    }
+  }
   render() {
+
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div
+          onClick={() => this.setState({ spin: !this.state.spin })}
+          className={`${this.state.spin ? "spin" : null} thing-1`} />
       </div>
-    );
+    )
   }
 }
-
-export default App;
