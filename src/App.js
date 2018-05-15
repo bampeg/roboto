d4e4b511022222dbfaea2a9670f4b6234bd395b2
Courtenay <court.eccles@gmail.com>
2018-05-14 23:29:53 -0600

    chest and legs added

--- src/App.js
+++ src/App.js
@@ -14,10 +14,22 @@ export default class App extends Component {
 
     return (
       <div className="App">
+        <h1>MR ROBOTO</h1>
         <div
           onClick={() => this.setState({ spin: !this.state.spin })}
-          className={`${this.state.spin ? "spin" : null} thing-1`}
-        />
+          className={`${this.state.spin ? "spin" : null} head`}>
+
+          <div className="eye" />
+          <div className="eye" />
+        </div>
+
+        <div className="chest" />
+
+        <div className="legs-container">
+          <div className="leg" />
+          <div className="leg" />
+        </div>
+
       </div>
     )
   }
